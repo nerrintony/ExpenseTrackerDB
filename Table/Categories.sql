c96d283d3b91519723f8e0a365813396a67631eb
nerrin <nirrentony@gmail.com>
2026-02-03 14:33:57 +0530

    category table

---
+++ Table/Categories.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Categories]
+(
+	[Id] INT NOT NULL PRIMARY KEY Identity(1,1),
+	[UserID] INT Not Null,
+	[Name] Varchar(50) Not null,
+	[Type] VARCHAR(20) NOT NULL
+        CHECK ([Type] IN ('income', 'expense')),
+	[CreatedAt] DATETIME NOT NULL DEFAULT GETDATE(),
+
+	CONSTRAINT FK_Categories_User
+        FOREIGN KEY ([UserID]) REFERENCES [dbo].[User]([Id])
+)
