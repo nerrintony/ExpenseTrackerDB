16d0d12e5fd0d24288822911d99bed17032a2ee0
nerrin <nirrentony@gmail.com>
2026-02-03 14:47:21 +0530

    Tansaction table

---
+++ Table/Transactions.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Transactions]
+(
+	[Id] INT NOT NULL PRIMARY KEY Identity(1,1),
+	[UserID] INT NOT nULL, 
+	[CategoryID] INT Not null,
+	[Amount] Decimal(10,2) NOt null,
+	[Type] VARCHAR(20) NOT NULL
+        CHECK ([Type] IN ('income', 'expense')),
+	[Description] Varchar(255),
+	[TransactionDate] Date Not NUll,
+	[CreatedAt] DATETIME NOT NULL DEFAULT GETDATE(),
+
+	FOREIGN KEY (UserID) REFERENCES [User](id),
+    FOREIGN KEY (CategoryID) REFERENCES Categories(id)
+)
